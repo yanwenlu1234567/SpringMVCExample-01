60f1a72d3a7ecea3a8478aa513742841b1c65713
79884 <79884@DESKTOP-7A2P363>
2023-12-03 23:14:49 +0900

    commit

--- src/main/resources/static/js/country.js
+++ src/main/resources/static/js/country.js
@@ -10,16 +10,29 @@ $(document).ready(function () {
         $("#detailContains").css("display", "block");
         // hide the queryContainer
         $("#queryContainer").css("display", "none");
+        $("#frmDetail").attr("action", "/country/createCountry");
+        $("#operation").val('create');
     });
 
     // when click the update button, show the queryContainer
-    $("#selUpdate, #selDelete").on('click', function () {
+    $("#selUpdate").on('click', function () {
         // show the queryContainer
         $("#queryContainer").css("display", "block");
         // hide the detailContains
         $("#detailContains").css("display", "none");
         // set the form action for update
-        $("#frmDetail").attr("action", "/UpdateCountry");
+        $("#frmDetail").attr("action", "/country/updateCountry");
+        $("#operation").val('update');
+    });
+
+    $("#selDelete").on('click', function () {
+        // show the queryContainer
+        $("#queryContainer").css("display", "block");
+        // hide the detailContains
+        $("#detailContains").css("display", "none");
+        // set the form action for update
+        $("#frmDetail").attr("action", "/country/deleteCountry");
+        $("#operation").val('delete');
     });
 
     // when click the return button, hide the detailContains
@@ -44,6 +57,37 @@ $(document).ready(function () {
                 // show the data in the detailContains
                 $("#countryCodeInput").val(data.mstcountrycd);
                 $("#countryNameInput").val(data.mstcountrynanme);
+            },
+            error: function (e) {
+                alert("error");
+            }
+        });
+    });
+
+    $("#updateBtn").on('click', function () {
+        opt = operation.value;
+        if (opt === "create") {
+		    request_url = "/country/createCountry";
+		    request_type = "POST";
+        }
+        else if (opt === "update") {
+            request_url = "/country/updateCountry";
+            // disabled the pk item
+            //document.getElementsById("countryCodeInput").setAttribute("disabled",true);
+            request_type = "POST";
+        }
+        else if (opt === "delete") {
+            request_url = "/country/deleteCountry";
+            request_type = "DELETE";
+        }
+
+        $.ajax({
+            type: request_type,
+            url: request_url,        //  <- controller function name
+            data: $("#frmDetail").serialize(),
+            dataType: 'json',
+            success: function (data) {
+                alert("success");
             },
             error: function (e) {
                 alert("error");
